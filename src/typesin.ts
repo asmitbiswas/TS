5b0afc160c00211043032cd12cfb16831eb395ef
Asmit Biswas <asmitbiswas@gmail.com>
2026-04-20 10:27:12 +0530

    todays commit

---
+++ src/typesin.ts
@@ -0,0 +1,12 @@
+let drink = "chai" // type infernce
+
+let cups = Math.random() > 0.5 ? 10: 5;
+
+let channelName = "Coffeandcode"
+
+let chaiFlavour:String = "masala tea"
+chaiFlavour = "ginger tea"
+
+// chaiFlavour = 2 invalid cause 2 isn't string
+//  annotation === we have to tell what to do in annotation
+// infernce === we haven't to tell nothing, it understands automatically
