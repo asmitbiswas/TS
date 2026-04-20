6cde81d8cddcf0acfc8beb2ccdf574fe29f44f6e
Asmit Biswas <asmitbiswas@gmail.com>
2026-04-20 11:02:20 +0530

    Union and dany

--- src/typesin.ts
+++ src/typesin.ts
@@ -7,6 +7,8 @@ let channelName = "Coffeandcode"
 let chaiFlavour:String = "masala tea"
 chaiFlavour = "ginger tea"
 
+let chaiOrder:boolean
+
 // chaiFlavour = 2 invalid cause 2 isn't string
 //  annotation === we have to tell what to do in annotation
 // infernce === we haven't to tell nothing, it understands automatically
---
+++ src/union_dany.ts
@@ -0,0 +1,23 @@
+// union is used when a datatype's value  ===  different on places ( exp : 3 & 5 line ; 9 & 11)
+
+let subs: string | number = '1M'
+
+let apiReq: 'success' | 'pending' | 'error' = "success"
+
+apiReq = "pending"
+
+let airLineSeat : 'aisle' | 'middle' | 'window' = "aisle"
+
+airLineSeat = "middle"
+
+let orders = ['65' ,  '66' , '67', '68' , '69']
+
+// let currentOrder;
+
+// for(let order of orders) {
+//     if(currentOrder === '67')
+//         currentOrder = order
+//         break;
+// }
+
+//  in this situation written above is any. here currentOrder is any ( it can be anything ) thats why we have to avoid using any. :)
