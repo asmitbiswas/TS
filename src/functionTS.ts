081eb82959e418dedfa32e3af0370bdfbe936ad7
asmitbiswas <asmitbiswas1308@gmail.com>
2026-04-30 11:23:51 +0530

    topic: functions ^-^

---
+++ src/functionTS.ts
@@ -0,0 +1,31 @@
+// (^-^) 
+
+function makeChai(type: string, cups: number){
+    console.log(`Making ${type} chai and ${cups} cups`)
+}
+
+makeChai( "MasalaChai", 2)
+
+function chaiPrice():number{
+    return 25
+}
+
+function makeOrder(order: string){
+    if(!order) return null
+    return order
+}
+
+function logChai():void{
+    console.log("nahh")
+}
+
+function orderChai(type?: string ){
+    console.log()
+}
+
+function createChai(order:{
+    name: string
+    price: number
+}){
+  console.log()  
+}
