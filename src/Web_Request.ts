60ee5fc2dff74c8b938f936889baf908bf0c369a
asmitbiswas <asmitbiswas1308@gmail.com>
2026-05-04 11:53:09 +0530

    Backend

--- src/Web_Request.ts
+++ src/Web_Request.ts
@@ -1,4 +1,5 @@
-import axios, {AxiosResponse} from "axios";
+import axios from "axios";
+import type {AxiosResponse} from "axios"
 
 axios.get( 'http://localhost:8080/data').then(res => {
     console.log(res.data)
